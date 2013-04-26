af7dfcf1f1ce78e1b19b71399a45f3fe53011c2f
Ross Miller <rgmiller@ornl.gov>
2013-04-26 13:33:18 -0400

    First checkin of the code.  Nowhere near feature complete and in fact totally untested on the DDN hardware.

---
+++ src/SFATimeSeries.py
@@ -0,0 +1,105 @@
+'''
+Created on 12 April, 2013
+
+@author: xmr
+'''
+
+import time
+
+class SFATimeSeries( object):
+    '''
+    Used to hold time series data (specifically from the DDN SFA controllers)
+    
+    Most of the SFA API queries return the number of things since the controller
+    was rebooted.  As such, they are expected to be continuously increasing.  Thus,
+    to compute for example the average read iops over the last 10 minutes, we only
+    need to subtract the most recent value from the one 10 minutes ago (and then
+    divide by 600 to get the I/O's per second).  We specifically *don't* need to 
+    look at all the values in between.
+    
+    In truth, if we knew in advance what time spans we'll want to calculate averages
+    for, we wouldn't have to keep all the values in between...
+    '''
+    
+    def __init__(self, max_size = None):
+        self._series = []
+        self._max_size = max_size
+    
+    def size(self):
+        '''
+        Returns the number of items in the series
+        '''
+        return len(self._series)
+    
+    def get(self, item_num):
+        '''
+        Returns a tuple of (value, timestamp) for the requested item
+        
+        Throws an IndexError if item_num is out of range
+        '''
+        return self._series[item_num]
+    
+    def flush(self):
+        '''
+        Delete all the values from the series
+        '''
+        self._series = []
+    
+    def average(self, span):
+        '''
+        Computes the average value over the last 'span' seconds
+        
+        Computes average "things per second" (where things are I/O Ops, bytes written,
+        etc...) over the requested span.  Returns a tuple of the computed value and
+        the actual span of seconds that it covered.  (For example, if values are added
+        every 2 seconds, but a 5 second average is requested, the actual span will be
+        4 seconds.)
+        '''
+        last_index = len(self._series) - 1
+        first_index = self._binary_search( self._series[last_index][1] - span)
+        average = ((self._series[last_index][0] - self._series[first_index][0]) /
+                   (self._series[last_index][1] - self._series[first_index][1]))
+        average = abs( average)
+        return (average, self._series[last_index][1] - self._series[first_index][1]) 
+    
+    def append(self, value):
+        '''
+        Adds one value to the time series and - if the max size has been
+        exceeded - drops the oldest value.
+        '''
+        
+        self._series.append((value, time.time()))
+        
+        # Remove any values that are too old
+        if ( self._max_size and (len(self._series) > self._max_size)):
+            self._series = self._series[len(self._series) - self._max_size:]
+                            
+        
+    def _binary_search(self, timeval):
+        '''
+        Search the data series and return the (value, time) tuple that is
+        closest to the requested time.
+        '''
+        # Can't use the standard bisect module because we have a series
+        # of tuples, not individual values...
+        min_index = 0;
+        max_index = len(self._series) - 1
+        # loop until we've narrowed it down to the two indexes that bracket our desired time value
+        while (max_index - min_index) > 1:
+            m = (min_index + max_index) / 2
+            if self._series[m][1] < timeval:
+                min_index = m
+            elif self._series[m][1] > timeval:
+                max_index = m
+            else:  
+                # we found an exact match for the time value
+                # this will almost never happen...
+                return m
+        
+        # now pick the index with the closer time value
+        if (timeval - self._series[min_index][1]) < (self._series[max_index][1] - timeval):
+            return min_index
+        else:
+            return max_index
+        
+    
---
+++ src/SFATimeSeries_Test.py
@@ -0,0 +1,106 @@
+'''
+Created on Apr 12, 2013
+
+@author: xmr
+'''
+
+import unittest
+import time
+from SFATimeSeries import SFATimeSeries
+
+# Constants used for building the series data
+#VALUE_START=1       # Starting value when adding data to the series
+#VALUE_INCREMENT=1   # Amount the series value will increase for each append
+#SERIES_SIZE=500     # Number of values to append to the series
+#APPEND_RATE=0.01     # Time (in seconds) between calls to append()
+
+SERIES_LENGTH=5.0   # Amount of time (in seconds) that the series should span
+SERIES_SIZE=500   # Approximate number of entries in the series
+                    # (This is approximate: the cutoff is actually the timespan)
+SERIES_RATE=1.0     # Average rate for the series data (in units/sec).
+                    # Calls to average() should return values very close to this
+
+class SFATimeSeries_Test( unittest.TestCase):
+    
+    def generate_time_series(self):
+        # Build up a time series that we can mess with...
+        print
+        print "Generating time series data.  This will take approximately %d seconds."%SERIES_LENGTH
+        self._series = SFATimeSeries()
+        approx_inter_item_time = SERIES_LENGTH / SERIES_SIZE
+        start_time = time.time()
+        append_val = 1
+        self._series.append( append_val)
+        while time.time() < (start_time + SERIES_LENGTH):
+            last_append_time = self._series.get( self._series.size()-1)[1]
+            while time.time() < (last_append_time + approx_inter_item_time):
+                time.sleep( approx_inter_item_time / 100)
+            
+            append_val += SERIES_RATE * (time.time() - last_append_time)     
+            self._series.append( append_val)
+                
+        print "Time series generated."
+
+    def testAppend(self):
+        SERIES_SIZE=500     # Number of values to append to the series
+        START_VALUE = 10
+        INCREMENT = 1
+        
+        local_series = SFATimeSeries(SERIES_SIZE)
+        value = START_VALUE
+        while local_series.size() < SERIES_SIZE:
+            orig_size = local_series.size()
+            local_series.append(value)
+            value += INCREMENT
+            self.assertEqual(orig_size+1, local_series.size(), "Append to series failed.  Series size = %d"%local_series.size())
+            
+        # series is now full. Verify first & last elements
+        self.assertEqual(local_series.size(), SERIES_SIZE,
+                          "Series was not full when it should have been.  Actual size: %d  Expected size: %d"%(local_series.size(), SERIES_SIZE))
+        self.assertEqual(local_series.get(0)[0], START_VALUE, "Unexpected first value in the series.  Did we remove it during an append?")
+        self.assertEqual(local_series.get(SERIES_SIZE-1)[0], value - INCREMENT,
+                          "Unexpected first value in the series.  Did we remove it during an append?")
+        
+        
+        # Add one more value and make sure the size stays at its max 
+        local_series.append( value)
+        value += INCREMENT
+        self.assertEqual(local_series.size(), SERIES_SIZE,
+                          "Unexpected series size.  Actual size: %d  Expected size: %d"%(local_series.size(), SERIES_SIZE))
+        self.assertEqual(local_series.get(0)[0], START_VALUE+INCREMENT,
+                         "Unexpected first value in the series.  Did we remove it during an append?")
+        self.assertEqual(local_series.get(SERIES_SIZE-1)[0], value - INCREMENT,
+                          "Unexpected first value in the series.  Did we remove it during an append?")
+        
+
+    def testFlush(self):
+        # Note: make sure testAppend runs first because we need append() for this test
+        SERIES_SIZE=50    
+        local_series = SFATimeSeries()
+        for i in range(SERIES_SIZE):
+            local_series.append( 1)
+        
+        self.assertEqual(local_series.size(), SERIES_SIZE, "Expected the series to be full.  It wasn't.")
+        local_series.flush()
+        self.assertEqual(local_series.size(), 0, "Expected the series to be empty.  It contained %d items."%local_series.size())
+                
+    # verify the average() function    
+    def testAverage(self):
+        # Note:  Make sure this test runs *AFTER* the append and flush tests, because
+        # we need both to actually work properly in order to run this test...
+        
+        self.generate_time_series()
+        
+        half_time =  SERIES_LENGTH / 2
+        result = self._series.average( half_time) # use half the time series
+        self.assertAlmostEqual( result[1], half_time, delta=(SERIES_LENGTH / self._series.size()),
+                                msg="Actual timespan for the Average() function too far from the requested timespan." )
+        
+        self.assertAlmostEqual(result[0], SERIES_RATE, delta=SERIES_RATE*.005)  # accurate to 0.5%
+        
+        
+if __name__ == '__main__':
+    unittest.main()
+        
+        
+        

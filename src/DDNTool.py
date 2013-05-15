ff7367bf8d7ce5160f050893fcc82906d8134421
Ross Miller <rgmiller@ornl.gov>
2013-05-15 11:52:39 -0400

    Changed the code to ensure the background threads are shutdown even if the main thread throws an exception.  The change also means we don't need the explicit signal handler any more.

--- src/DDNTool.py
+++ src/DDNTool.py
@@ -11,7 +11,6 @@ import ConfigParser
 import sys
 import time
 import argparse
-import signal
 
 import SFAClient
 import SFADatabase
@@ -29,33 +28,42 @@ DEFAULT_CONF_FILE="./ddntool.conf"  # config file to use if not specified on the
 
 
 # list of SFAClient objects.  Have to make this global so that
-# the signal handler can access it to shut the threads down
+# the shutdown function can access it to shut the threads down
 sfa_clients=[]
 
-# Define a signal handler for SIGINT so that we can shut down cleanly.
-# (The main function is designed to loop forever.)
-def sigint_handler( signum, frame):
-    assert signum != signal.SIGINT
+class Thread_Shutdown_Exception(Exception):
+    '''
+    An exception for indicating a problem shutting
+    down a background thread.
+    '''
+    pass # Don't need anything other than what's in the base class
 
-    print "SIGINT detected.  Shutting down."
+def clean_shutdown():
+    '''
+    Shut down the background thread(s) cleanly
+
+    Returns nothing, but does not return until the threads have stopped.
+    Raises a Thread_Shutdown_Exception exception if a thread fails
+    to stop in a reasonable amount of time.
+    '''
+
     # request each client to stop (using the option to
     # return immediately so we can get all the requests
     # in quickly
     for c in sfa_clients:
         c.stop_thread( False)
 
-
     # Send the thread stop request again, this time waiting
     # (hopefully not long) for the threads to exit
     for c in sfa_clients:
         if c.stop_thread( True, 10) == False:
-            print "Failed to stop background thread for client %s"%c.get_host_name()
-            print "Attempting to exit anyway."
-    
-    exit()
+            raise Thread_Shutdown_Exception, \
+                "Failed to stop background thread for client %s"%c.get_host_name()
 
-########## End of sigint_handler() ##########################
-     
+
+########## End of clean_shutdown() ##########################
+
+
 
 def main_func():
     
@@ -83,47 +91,46 @@ def main_func():
     config = ConfigParser.ConfigParser()
     config.read(args.conf_file)
     
-    # Connect to the DDN hardware
-    sfa_user = config.get('ddn_hardware', 'sfa_user')
-    sfa_password = config.get('ddn_hardware', 'sfa_password')
-    sfa_hosts = [ host.strip() for host in config.get('ddn_hardware', 'sfa_hosts').split(",") ]
-    for host in sfa_hosts:
-        client = SFAClient.SFAClient( host, sfa_user, sfa_password)
-        sfa_clients.append( client)
+    try:
+        # Connect to the DDN hardware
+        sfa_user = config.get('ddn_hardware', 'sfa_user')
+        sfa_password = config.get('ddn_hardware', 'sfa_password')
+        sfa_hosts = [ host.strip() for host in config.get('ddn_hardware', 'sfa_hosts').split(",") ]
+        for host in sfa_hosts:
+            client = SFAClient.SFAClient( host, sfa_user, sfa_password)
+            sfa_clients.append( client)
 
-    time.sleep(5)
-    if not client.is_connected():
-        print "Failed to connect to SFA Host %s"%host
+        time.sleep(5)
+        if not client.is_connected():
+            print "Failed to connect to SFA Host %s"%host
      
-    # Connect to the database
-    db_user = config.get('database', 'db_user')
-    db_password = config.get('database', 'db_password')
-    db_host = config.get('database', 'db_host')
-    db_name = config.get('database', 'db_name')
-    db = SFADatabase.SFADatabase(db_user, db_password, db_host, db_name, args.init_db)
-    db.verify_main_table( sfa_hosts)
+        # Connect to the database
+        db_user = config.get('database', 'db_user')
+        db_password = config.get('database', 'db_password')
+        db_host = config.get('database', 'db_host')
+        db_name = config.get('database', 'db_name')
+        db = SFADatabase.SFADatabase(db_user, db_password, db_host, db_name, args.init_db)
+        db.verify_main_table( sfa_hosts)
 
+        # install the signal handler
+        #signal.signal( signal.SIGINT, sigint_handler)
 
-    # install the signal handler
-    signal.signal( signal.SIGINT, sigint_handler)
-
-
-    # loop forever polling the clients and updating the database
-    print "Entering main loop.  Ctrl-C to exit."
-    while True:
-        time.sleep(5) 
-        for client in sfa_clients:
-            read_iops = client.get_read_iops( 0, 5)
-            write_iops = client.get_write_iops( 0, 5)
-            bandwidth = client.get_transfer_bw( 0, 5)
-            db.update_main_table(client.get_host_name(), bandwidth, read_iops, write_iops, 0, 0)
-
+        # loop forever polling the clients and updating the database
+        print "Entering main loop.  Ctrl-C to exit."
+        while True:
+            time.sleep(5) 
+            for client in sfa_clients:
+                read_iops = client.get_read_iops( 0, 5)
+                write_iops = client.get_write_iops( 0, 5)
+                bandwidth = client.get_transfer_bw( 0, 5)
+                db.update_main_table(client.get_host_name(), bandwidth, read_iops, write_iops, 0, 0)
+    except KeyboardInterrupt:
+        # Perfectly normal.  Ctrl-C is how we expect to exit
+        pass
+    finally:
+        print "Shutting down background threads."
+        clean_shutdown()
         
-    # Should never get here!
-    print "Unexpected exit from the main loop.  This shouldn't ever happen!"
-    sigint_handler( 2, None) # call the signal handler directly (that's
-                             # where all the cleanup is done anyway)
-
 ############ End of main_func() #####################
 
 if __name__ == '__main__':

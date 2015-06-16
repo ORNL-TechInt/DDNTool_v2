d4f4ccd8b2899ef37fa758b4f5dacac2d1b2631c
Ross Miller <rgmiller@ornl.gov>
2015-06-16 09:44:46 -0400

    Changes to setup.py
    Fix a typo in one of the filenames, bump the version to 2.1 and increase
    the minimum ddn.sfa.api version

--- setup.py
+++ setup.py
@@ -9,17 +9,16 @@ Setup script for python distutils
 
 from distutils.core import setup
 
-
 setup (
    name         = "DDNTool",
    description  = "A tool for monitoring performance of multiple DDN SFA controllers", 
    author       = "Ross Miller",
    author_email = "rgmiller@ornl.gov",
    
-   version      = "2.0Beta",
+   version      = "2.1",
    url          = "http://www.olcf.ornl.gov", # don't have a web page dedicated to the tool
 
-   requires     = ["ddn.sfa.api (>= 1.5.3)"],
+   requires     = ["ddn.sfa.api (>= 2.3.0)"],
    
    package_dir  = {"":"src"},
    py_modules   = ["bracket_expand"],
@@ -27,5 +26,5 @@ setup (
    
    scripts      = ["src/DDNTool.py" ], # scripts list isn't affected by the package_dir dict
 
-   data_files	= [('/etc/init', ['src/init/DDNTool.conf']),('/etc/', ['src/ddntool.sample.conf'])] # this is the upstart config file and sample configuration file
+   data_files	= [('/etc/init', ['src/init/DDNTool.conf']),('/etc/', ['src/ddntool.conf.sample'])] # this is the upstart config file and sample configuration file
 )

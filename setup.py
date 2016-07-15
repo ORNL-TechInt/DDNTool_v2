dfb190175246da71405b9c56ac21aff7ab4d22d5
Ross Miller <rgmiller@ornl.gov>
2016-07-15 16:27:59 -0400

    Changes to setup.py
    Moving from distutils to setuptools.  Added trove classifiers.  Also added
    some checks for the version of Linux we're building the package for.

--- setup.py
+++ setup.py
@@ -22,7 +22,39 @@
 Setup script for python distutils
 '''
 
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup, find_packages
+import platform
+import sys
+
+def get_distro():
+    '''
+    Returns a tuple consisting of the distro name (string) and major number (int)
+    '''
+    (distro, version, none) = platform.dist()
+    majorVersion = int(version.split('.')[0])
+    return (distro, majorVersion)
+
+
+# Do some sanity checks on the distribution we're packaging for
+(distro, version) = get_distro()
+if not (distro == "redhat" or \
+        distro == "centos"):
+    raise RuntimeError( "Unknown Linux distribution: '%s'. "%distro +
+                        "We can only build packages for RedHat Enterprise " +
+                        "Linux and CentOS.")
+    
+if version < 6:
+    raise RuntimeError( "Linux distribution too old.  We need at least v6.")
+
+if version > 7:
+    raise RuntimeError( "Linux distribution too new.  v7 is the latest " +
+                        "currently supported.")
+
+if sys.version_info[:3] < (2,6,0):
+    raise RuntimeError("This application requires Python 2.6 or 2.7")
+
+
 
 setup (
    name         = "DDNTool",
@@ -32,7 +64,14 @@ setup (
    
    version      = "2.4.1",
    url          = "https://github.com/ORNL-TechInt/DDNTool_v2",
-   # the github page is a close to a home page for the tool as we've got
+   # the github page is as close to a home page for the tool as we've got
+   
+   classifiers  = [        
+      'Intended Audience :: System Administrators',
+      'Operating System :: POSIX',
+      'Programming Language :: Python :: 2.6',
+      'Programming Language :: Python :: 2.7'
+   ],
 
    requires     = ["ddn.sfa.api (>= 2.3.0)"],
    

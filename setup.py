7055258eba8f07e963befbee5f7d52cb49286944
Ross Miller <rgmiller@ornl.gov>
2016-07-15 17:08:01 -0400

    PEP8 cleanups for setup.py
    Ran a PEP8 checker over setup.py, but I'm deliberately ignoring the warnings
    about too many space before the assignment operator because I want all the
    assignments in the setup() arguements list to line up nicely.
    
    Also changed how we handle the returned values from platform.dist()\

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 # Created on Aug 2, 2013
-# 
+#
 # @author: Ross Miller
-# 
+#
 # Copyright 2013, 2015 UT Battelle, LLC
-# 
+#
 # This work was supported by the Oak Ridge Leadership Computing Facility at
 # the Oak Ridge National Laboratory, which is managed by UT Battelle, LLC for
 # the U.S. DOE (under the contract No. DE-AC05-00OR22725).
-# 
+#
 # This file is part of DDNTool_v2.
-# 
+#
 # DDNTool_v2 is free software: you can redistribute it and/or modify it under
 # the terms of the UT-Battelle Permissive Open Source License.  (See the
 # License.pdf file for details.)
-# 
+#
 # DDNTool_v2 is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
 # A PARTICULAR PURPOSE.
@@ -27,59 +27,64 @@ from setuptools import setup, find_packages
 import platform
 import sys
 
+
 def get_distro():
     '''
-    Returns a tuple consisting of the distro name (string) and major number (int)
+    Returns a tuple consisting of the distro name (string) and major
+    number (int)
     '''
-    (distro, version, none) = platform.dist()
+    distro, version = platform.dist()[0:2]
     majorVersion = int(version.split('.')[0])
     return (distro, majorVersion)
 
 
 # Do some sanity checks on the distribution we're packaging for
 (distro, version) = get_distro()
-if not (distro == "redhat" or \
+if not (distro == "redhat" or
         distro == "centos"):
-    raise RuntimeError( "Unknown Linux distribution: '%s'. "%distro +
-                        "We can only build packages for RedHat Enterprise " +
-                        "Linux and CentOS.")
-    
+    raise RuntimeError("Unknown Linux distribution: '%s'. " % distro +
+                       "We can only build packages for RedHat Enterprise " +
+                       "Linux and CentOS.")
+
 if version < 6:
-    raise RuntimeError( "Linux distribution too old.  We need at least v6.")
+    raise RuntimeError("Linux distribution too old.  We need at least v6.")
 
 if version > 7:
-    raise RuntimeError( "Linux distribution too new.  v7 is the latest " +
-                        "currently supported.")
+    raise RuntimeError("Linux distribution too new.  v7 is the latest " +
+                       "currently supported.")
 
-if sys.version_info[:3] < (2,6,0):
+if sys.version_info[:3] < (2, 6, 0):
     raise RuntimeError("This application requires Python 2.6 or 2.7")
 
 
+setup(
+    name         = "DDNTool",
+    description  = "A tool for monitoring performance of multiple " +
+                   "DDN SFA controllers",
+    author       = "Ross Miller",
+    author_email = "rgmiller@ornl.gov",
 
-setup (
-   name         = "DDNTool",
-   description  = "A tool for monitoring performance of multiple DDN SFA controllers", 
-   author       = "Ross Miller",
-   author_email = "rgmiller@ornl.gov",
-   
-   version      = "2.4.1",
-   url          = "https://github.com/ORNL-TechInt/DDNTool_v2",
-   # the github page is as close to a home page for the tool as we've got
-   
-   classifiers  = [        
-      'Intended Audience :: System Administrators',
-      'Operating System :: POSIX',
-      'Programming Language :: Python :: 2.6',
-      'Programming Language :: Python :: 2.7'
-   ],
+    version      = "2.4.1",
+    url          = "https://github.com/ORNL-TechInt/DDNTool_v2",
+    # the github page is as close to a home page for the tool as we've got
 
-   requires     = ["ddn.sfa.api (>= 2.3.0)"],
-   
-   package_dir  = {"":"src"},
-   py_modules   = ["bracket_expand"],
-   packages     = ["SFAClientUtils" ],
-   
-   scripts      = ["src/DDNTool.py" ], # scripts list isn't affected by the package_dir dict
+    classifiers  = [
+        'Intended Audience :: System Administrators',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ],
 
-   data_files	= [('/etc/init', ['src/init/DDNTool.conf']),('/etc/', ['src/ddntool.conf.sample'])] # this is the upstart config file and sample configuration file
+    requires     = ["ddn.sfa.api (>= 2.3.0)"],
+
+    package_dir  = {"": "src"},
+    py_modules   = ["bracket_expand"],
+    packages     = ["SFAClientUtils"],
+
+    # scripts list isn't affected by the package_dir dict
+    scripts      = ["src/DDNTool.py"],
+
+    # this is the upstart config file and sample configuration file
+    data_files   = [('/etc/init', ['src/init/DDNTool.conf']),
+                    ('/etc/', ['src/ddntool.conf.sample'])]
 )

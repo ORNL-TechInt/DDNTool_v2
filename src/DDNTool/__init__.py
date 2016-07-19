827e028ad9718fbf2a163bd7676da85eef43bb06
Ross Miller <rgmiller@ornl.gov>
2016-07-19 14:12:01 -0400

    Forgot to add __init__.py when moving everything into the DDNTool package
    Should have been added a few commits ago...

---
+++ src/DDNTool/__init__.py
@@ -0,0 +1,9 @@
+# The __init__.py file for the DDNTool package.
+
+'''
+This package contains functions and classes used by DDNTool.py.  They're not
+particularly useful anywhere else.
+'''
+
+# pull in the 'public' functions from bracket_expand.py
+from bracket_expand import bracket_aware_split, bracket_expand
